3b41dd95d5bcde6dfe505b3f9d2df0845496653c
Luis Ernesto Hernandez Rivera <ernes@DESKTOP-D1ICO8M>
2022-05-20 12:01:50 -0600

    compilación de varios scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'))
 
 function css(done) {
   //identificar el archivo sass
-  const archivo = src('src/scss/App.scss')
+  const archivo = src('src/scss/**/*.scss')
   //compilarlo
   archivo.pipe(sass())
   //almacenarla en el disco duro
@@ -13,7 +13,7 @@ function css(done) {
 }
 
 function dev(done) {
-  watch('src/scss/App.scss', css)
+  watch('src/scss/**/*.scss', css)
   done()
 }
 
