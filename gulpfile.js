7572e87871dca09fbd42697da97f113c2057d6f0
Luis Ernesto Hernandez Rivera <ernes@DESKTOP-D1ICO8M>
2022-05-24 17:12:38 -0600

    convirtiendo a formatos ligeros imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
+//CSS
 const sass = require('gulp-sass')(require('sass'))
 const plumber = require('gulp-plumber')
+//Imagenes
+const avif = require('gulp-avif')
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
 
 function css(done) {
   //identificar el archivo sass
@@ -13,10 +19,49 @@ function css(done) {
   done()
 }
 
+function imagenes(done){
+  const opciones = {
+    optiizationLevel: 3
+  }
+  src('src/img/**/*.{png,jpg}')
+  .pipe(cache(imagemin(opciones)))
+  .pipe(dest('build/img'))
+  done()
+}
+
+function versionWebp(done){
+  const opciones = {
+    quality: 50
+  };
+  //identifica los archivos de imagen
+  src('src/img/**/*.{png,jpg}')
+  //convierte
+  .pipe( webp(opciones))
+  //guarda
+  .pipe( dest('build/img'))
+  done()
+}
+
+function versionAvif(done){
+  const opciones = {
+    quality: 50
+  };
+  //identifica los archivos de imagen
+  src('src/img/**/*.{png,jpg}')
+  //convierte
+  .pipe( avif(opciones))
+  //guarda
+  .pipe( dest('build/img'))
+  done()
+}
+
 function dev(done) {
   watch('src/scss/**/*.scss', css)
   done()
 }
 
-exports.css = css
-exports.dev = dev
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
