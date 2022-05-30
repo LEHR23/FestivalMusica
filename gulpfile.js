010ba06604a8e342d2969c9b2f7e8e4edd4290d6
Luis Ernesto Hernandez Rivera <ernes@DESKTOP-D1ICO8M>
2022-05-29 22:23:28 -0600

    minificando css y js

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,23 @@ const { src, dest, watch, parallel } = require('gulp')
 //CSS
 const sass = require('gulp-sass')(require('sass'))
 const plumber = require('gulp-plumber')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const postcss = require('gulp-postcss')
 //Imagenes
 const avif = require('gulp-avif')
 const cache = require('gulp-cache')
 const imagemin = require('gulp-imagemin')
 const webp = require('gulp-webp')
+//JavaScript
+const terser = require('gulp-terser-js')
 
 function css(done) {
   //identificar el archivo sass
   const archivo = src('src/scss/**/*.scss').pipe(plumber())
   //compilarlo
   archivo.pipe(sass())
+  .pipe(postcss([ autoprefixer(), cssnano() ]))
   //almacenarla en el disco duro
   archivo.pipe(dest('build/css'))
   //finaliza la tarea
@@ -57,6 +63,7 @@ function versionAvif(done){
 
 function javascript(done) {
   src('src/js/**/*.js').
+    pipe(terser()).
     pipe(dest('build/js'))
   done()
 }
